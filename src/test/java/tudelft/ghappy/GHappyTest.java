6374549654f68a397e3ea96bfcf72f7cb0391231
DGA_PC <dgarrido@imaster.golf>
2021-11-06 14:19:12 +0100

    Modificaciones sobre GHappyTest

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,41 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void onegHappy() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxggxx");
+                Assertions.assertEquals(result, true);
+    }
+
+    @Test
+    public void gNotHappy() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxgxx");
+        Assertions.assertEquals(result, false);
+    }
+
+    @Test
+    public void AllgNotHappy() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxggyygxx");
+        Assertions.assertEquals(result, false);
+    }
+
+    @Test
+    public void Notg() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("xxxxxx");
+        Assertions.assertEquals(result, false);
+    }
+
+    @Test
+    public void voidString() {
+        GHappy gh = new GHappy();
+        boolean result = gh.gHappy("");
+        Assertions.assertEquals(result, false);
+    }
 }
